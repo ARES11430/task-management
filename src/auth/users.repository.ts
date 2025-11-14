a9b71fe2ae3a95cc58bdddb223792a655060f121
Amin Ghasemi <amin11.ghasemi@gmail.com>
2025-11-14 20:00:12 +0330

    Sign up users

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { DataSource, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UsersRepository {
-  public readonly repo: Repository<User> & {};
+  public readonly repo: Repository<User> & {
+    createUser(authCredentials: AuthCredentialsDto): Promise<void>;
+  };
 
   constructor(private dataSource: DataSource) {
-    this.repo = this.dataSource.getRepository(User).extend({});
+    this.repo = this.dataSource.getRepository(User).extend({
+      async createUser(authCredentials: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentials;
+        const user = this.create({ username, password });
+        await this.save(user);
+      },
+    });
   }
 }
