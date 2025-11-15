9a2f4d80a84beb7374138eb0ec86d49bb461dda8
Amin Ghasemi <amin11.ghasemi@gmail.com>
2025-11-15 20:00:30 +0330

    Sign JWT on login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,31 @@
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UsersRepository) {}
+  constructor(
+    private userRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async sinUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.repo.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.repo.findOneBy({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'Signed in successfully';
+      const payload: JwtPayload = { username };
+      const accessToken = this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('Invalid credentials');
     }
