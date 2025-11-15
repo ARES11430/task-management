94dddc36405c3f7d49c09f1dbd03d943caecea7e
Amin Ghasemi <amin11.ghasemi@gmail.com>
2025-11-15 17:35:59 +0330

    Sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { UsersRepository } from './users.repository';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class AuthService {
@@ -8,5 +13,16 @@ export class AuthService {
 
   async sinUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.repo.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.repo.findOneBy({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'Signed in successfully';
+    } else {
+      throw new UnauthorizedException('Invalid credentials');
+    }
   }
 }
